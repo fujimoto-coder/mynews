766ce36b509257faef563ec29ea01a347afdf9f1
fujimoto <fujimoto.k@ozzio.jp>
2022-12-02 08:15:22 +0000

    21課題

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::controller(aaaController::class)->group(function() {
-//     Route::get('xxx', 'bbb');
-// });
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->group(function() {
-    Route::get('admin/profile/create', 'add');
+    Route::get('admin/profile/create', 'add')->middleware('auth');
 });
 Route::controller(ProfileController::class)->group(function() {
-    Route::get('admin/profile/edit', 'edit');
+    Route::get('admin/profile/edit', 'edit')->middleware('auth');
 });
 Auth::routes();
 
